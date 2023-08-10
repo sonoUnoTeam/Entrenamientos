d42210440789ede953dfe7d12b1bee37ac3ea00f
NatashaBertaina <112961022+NatashaBertaina@users.noreply.github.com>
2023-08-10 15:29:02 -0300

    Update formulario.js

--- javaScript/formulario.js
+++ javaScript/formulario.js
@@ -2,7 +2,17 @@ const para = document.querySelector('.p1');
 const para = document.querySelector('.p2');
 const para = document.querySelector('.p3');
 const para = document.querySelector('.p4');
+const btn = document.querySelector("button");
 
+/*const btn = document.querySelector("button");
+
+
+
+btn.addEventListener("click", () => {
+  const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+  document.body.style.backgroundColor = rndCol;
+});
+btn.addEventListener("click", changeBackground);*/
 
 let intentosCount = 1;
 
@@ -47,35 +57,4 @@ function rta() {
     else alert('No Attempts');
 }
 
-/*let countAttempts = 1;
-
-function rta()
-{
-
-    if(countAttempts == 1)
-    {
-        if (document.getElementById('senoidal1').checked) message = '<b><font color="#65FC03"> Correct! </font>';
-        else message = '<b><font color="#FC0303"> Ooh!! This is not the displayed function. </font></b>';
-
-        document.getElementById("p1").innerHTML = message;
-
-        if (document.getElementById('creciente2').checked) message = '<b><font color="#65FC03"> Correct! </font>';
-        else message = '<b><font color="#FC0303"> Ooh!! This is not the displayed function. </font></b>';
-
-        document.getElementById("p2").innerHTML = message;
-
-        if (document.getElementById('cuadrada3').checked) message = '<b><font color="#65FC03"> Correct! </font>';
-        else message = '<b><font color="#FC0303"> Ooh!! This is not the displayed function. </font></b>';
-
-        document.getElementById("p3").innerHTML = message;
-
-        if (document.getElementById('decreciente4').checked) message = '<b><font color="#65FC03"> Correct! </font></b>';
-        else message = '<b><font color="#FC0303"> Ooh!! This is not the displayed function. </font></b>';
-
-        document.getElementById("p4").innerHTML = message;
-
-        countAttempts = 0;
-    }
-    else alert('No Attempts');
-}
-*/
+btn.addEventListener("click", rta);
