0204783776beec75e9338d09658a4281e0a4fd04
NatashaBertaina <112961022+NatashaBertaina@users.noreply.github.com>
2023-08-10 13:41:11 -0300

    Update formulario.js

--- javaScript/formulario.js
+++ javaScript/formulario.js
@@ -4,7 +4,7 @@ const para = document.querySelector('.p3');
 const para = document.querySelector('.p4');*/
 
 
-let intentosCount = 1;
+/*let intentosCount = 1;
 
 function rta() {
     let mensaje;
@@ -40,4 +40,36 @@ function rta() {
         mensaje = 'Ooh!! This is not the displayed function.';
         document.getElementById('p4').innerHTML = mensaje;
     }
+}*/
+
+let countAttempts = 1;
+
+function rta()
+{
+
+    if(countAttempts == 1)
+    {
+        if (document.getElementById('senoidal1').checked) message = '<b><font color="#65FC03"> Correct! </font>';
+        else message = '<b><font color="#FC0303"> Ooh!! This is not the displayed function. </font></b>';
+
+        document.getElementById("p1").innerHTML = message;
+
+        if (document.getElementById('creciente2').checked) message = '<b><font color="#65FC03"> Correct! </font>';
+        else message = '<b><font color="#FC0303"> Ooh!! This is not the displayed function. </font></b>';
+
+        document.getElementById("p2").innerHTML = message;
+
+        if (document.getElementById('cuadrada3').checked) message = '<b><font color="#65FC03"> Correct! </font>';
+        else message = '<b><font color="#FC0303"> Ooh!! This is not the displayed function. </font></b>';
+
+        document.getElementById("p3").innerHTML = message;
+
+        if (document.getElementById('decreciente4').checked) message = '<b><font color="#65FC03"> Correct! </font></b>';
+        else message = '<b><font color="#FC0303"> Ooh!! This is not the displayed function. </font></b>';
+
+        document.getElementById("p4").innerHTML = message;
+
+        countAttempts = 0;
+    }
+    else alert('No Attempts');
 }
