79dc90d4bce96994a291ab4cf69ae3a3af1c15c7
NatashaBertaina <112961022+NatashaBertaina@users.noreply.github.com>
2023-08-10 13:22:16 -0300

    Update formulario.js

--- javaScript/formulario.js
+++ javaScript/formulario.js
@@ -7,38 +7,38 @@ const para = document.querySelector('.p4');*/
 let intentosCount = 1;
 
 function rta() {
-    let mensaje1 = '';
+/*    let mensaje1 = '';
     let mensaje2 = '';
     let mensaje3 = '';
-    let mensaje4 = '';
+    let mensaje4 = '';*/
 
     if (document.getElementById('senoidal1').checked){
-        mensaje1 = 'Correct!';
+        mensaje = 'Correct!';
     } else {
-        mensaje1 = 'Ooh!! This is not the displayed function.';
+        mensaje = 'Ooh!! This is not the displayed function.';
     }
-    document.getElementById("p1").innerHTML = mensaje1;
+    document.getElementById('p1').innerHTML = mensaje;
 
     if (document.getElementById('creciente2').checked){
-        mensaje2 = 'Correct!';
+        mensaje = 'Correct!';
     } else {
-        mensaje2 = 'Ooh!! This is not the displayed function.';
+        mensaje = 'Ooh!! This is not the displayed function.';
     }
-    document.getElementById("p2").innerHTML = mensaje1;
+    document.getElementById('p2').innerHTML = mensaje;
 
     if (document.getElementById('cuadrada3').checked){
-        mensaje3 = 'Correct!';
+        mensaje = 'Correct!';
     } else {
-        mensaje3 = 'Ooh!! This is not the displayed function.';
+        mensaje = 'Ooh!! This is not the displayed function.';
     }
-    document.getElementById("p3").innerHTML = mensaje1;
+    document.getElementById('p3').innerHTML = mensaje;
 
     if (document.getElementById('decreciente4').checked){
-        mensaje4 = 'Correct!';
+        mensaje = 'Correct!';
     } else {
-        mensaje4 = 'Ooh!! This is not the displayed function.';
+        mensaje = 'Ooh!! This is not the displayed function.';
     }
-    document.getElementById("p4").innerHTML = mensaje1;
+    document.getElementById('p4').innerHTML = mensaje;
 
     //para.textContent('Question 1:' + mensaje1 + '<br>Question 2:' + mensaje2 + '<br>Question 3:' + mensaje3 + 'Question 4:' + mensaje4);
 }
