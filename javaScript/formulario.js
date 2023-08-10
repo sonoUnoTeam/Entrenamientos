249781a5e2c1d70277a67ef9cd6cec058ff71f35
NatashaBertaina <112961022+NatashaBertaina@users.noreply.github.com>
2023-08-10 13:54:59 -0300

    Update formulario.js

--- javaScript/formulario.js
+++ javaScript/formulario.js
@@ -1,48 +1,48 @@
-/*const para = document.querySelector('.p1');
+const para = document.querySelector('.p1');
 const para = document.querySelector('.p2');
 const para = document.querySelector('.p3');
-const para = document.querySelector('.p4');*/
+const para = document.querySelector('.p4');
 
 
-/*let intentosCount = 1;
+let intentosCount = 1;
 
 function rta() {
     let mensaje;
     
-    if (document.getElementById('senoidal1').checked){
+    if (document.getElementById('#senoidal1').checked){
         mensaje = 'Correct!';
-        document.getElementById('p2').innerHTML = mensaje;
+        document.getElementById('.p1').innerHTML = mensaje;
     } else {
         mensaje = 'Ooh!! This is not the displayed function.';
-        document.getElementById('p2').innerHTML = mensaje;
+        document.getElementById('.p1').innerHTML = mensaje;
     }
     
-    if (document.getElementById('creciente2').checked){
+    if (document.getElementById('#creciente2').checked){
         mensaje = 'Correct!';
-        document.getElementById('p2').innerHTML = mensaje;
+        document.getElementById('.p2').innerHTML = mensaje;
     } else {
         mensaje = 'Ooh!! This is not the displayed function.';
-        document.getElementById('p2').innerHTML = mensaje;
+        document.getElementById('.p2').innerHTML = mensaje;
     }
     
-    if (document.getElementById('cuadrada3').checked){
+    if (document.getElementById('#cuadrada3').checked){
         mensaje = 'Correct!';
-        document.getElementById('p3').innerHTML = mensaje;
+        document.getElementById('.p3').innerHTML = mensaje;
     } else {
         mensaje = 'Ooh!! This is not the displayed function.';
-        document.getElementById('p3').innerHTML = mensaje;
+        document.getElementById('.p3').innerHTML = mensaje;
     }
     
-    if (document.getElementById('decreciente4').checked){
+    if (document.getElementById('#decreciente4').checked){
         mensaje = 'Correct!';
-        document.getElementById('p4').innerHTML = mensaje;
+        document.getElementById('.p4').innerHTML = mensaje;
     } else {
         mensaje = 'Ooh!! This is not the displayed function.';
-        document.getElementById('p4').innerHTML = mensaje;
+        document.getElementById('.p4').innerHTML = mensaje;
     }
-}*/
+}
 
-let countAttempts = 1;
+/*let countAttempts = 1;
 
 function rta()
 {
@@ -73,3 +73,4 @@ function rta()
     }
     else alert('No Attempts');
 }
+*/
